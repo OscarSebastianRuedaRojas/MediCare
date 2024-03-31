36edf0b882cc3be815c8f66398dca4180aecc872
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-03-30 23:37:49 -0500

    feat: :tada: Initial commit
    Se creo el repositorio con su respectivo nav

---
+++ app/component/nav/nav.js
@@ -0,0 +1,62 @@
+export class SideBar extends HTMLElement{
+    constructor(){
+        super();
+        this.render();
+    }
+    render(){
+        this.innerHTML = /* html */`
+        <style rel="stylesheet">
+          @import "./app/component/nav/nav.css";
+        </style>
+        <header>
+        <div class="logo">
+            <img src="storage/img/LOGO-FOSCAL-2020-removebg-preview.png" alt="">
+        </div>
+        <nav>
+            <a href="#" data-verocultar='["H"]'>Home</a>
+            <a href="#" data-verocultar='["PI"]'>Primera Infancia</a>
+            <a href="#" data-verocultar='["I"]'>Infancia</a>
+            <a href="#" data-verocultar='["A"]'>Adolescencia</a>
+            <a href="#" data-verocultar='["G"]'>Gestantes</a>
+            <a href="#" data-verocultar='["PL"]'>Planificacion</a>
+        </nav>
+        <div class="menu-icons">
+            <i id="menu" class='bx bx-menu'></i>
+        </div>
+    </header>
+        `;
+        this.querySelectorAll("a").forEach((val)=>{
+            val.addEventListener("click", (e)=>{
+                let data = JSON.parse(e.target.dataset.verocultar)[0];
+                let mainContent = document.querySelector('#mainContent');
+                if (e.target.dataset.verocultar == '["Dn"]') {
+                    console.log("yes");
+                    mainContent.innerHTML= "<home-content></home-content>";
+                }
+                switch (data){
+                    case 'H':
+                        mainContent.innerHTML= "<form-register></form-register>";
+                        break;
+                    case 'PI':
+                        mainContent.innerHTML= "<form-marca></form-marca>";
+                        break;
+                    case 'I':
+                        mainContent.innerHTML= "<form-persona></form-persona>";
+                        break;
+                    case 'A':
+                        mainContent.innerHTML= "<form-estado></form-estado>";
+                        break;
+                    case 'G':
+                        mainContent.innerHTML= "<form-estado></form-estado>";
+                        break;
+                    case 'PL':
+                        mainContent.innerHTML= "<form-estado></form-estado>";
+                        break;
+                }
+                e.stopImmediatePropagation();
+                e.preventDefault();
+            })
+        })
+    }
+}
+customElements.define("sidebar-nav", SideBar);
